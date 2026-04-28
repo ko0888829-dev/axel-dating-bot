8f3f4b45de47f3875a5344cf335b5d210229e505
ko0888829-dev <ko0888829@gmail.com>
2026-04-28 14:15:32 +0630

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ from telegram.ext import Application, CommandHandler, MessageHandler, filters, C
 
 # --- CONFIG ---
 TOKEN = os.getenv("8639241153:AAGcL6T6bgJ1QdccyVb4fuLxq2qgTIm3wIo")
-MONGO_URI = os.getenv("mongodb+srv://hein:heinhein2007@cluster0.ehhc6my.mongodb.net/")
+MONGO_URI = "mongodb+srv://hein:heinhein2007@cluster0.ehhc6my.mongodb.net/"
 ADMIN_LIST = [int(id) for id in os.getenv("7311138952", "7097694897").split(",") if id]
 
 client = pymongo.MongoClient(MONGO_URI)
+
 db = client["axel_money_bot"]
 users_col = db["users"]
 tasks_col = db["tasks"]
